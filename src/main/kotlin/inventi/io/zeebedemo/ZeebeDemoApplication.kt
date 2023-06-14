7f37a1320de4dee8713d989f2554f428ad53bdde
Lukas Milašauskas <lukas.milasauskas@inventi.lt>
2023-06-14 16:59:21 +0300

    feat: initial commit

---
+++ src/main/kotlin/inventi/io/zeebedemo/ZeebeDemoApplication.kt
@@ -0,0 +1,11 @@
+package inventi.io.zeebedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZeebeDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZeebeDemoApplication>(*args)
+}
---
+++ src/test/kotlin/inventi/io/zeebedemo/ZeebeDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package inventi.io.zeebedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZeebeDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
